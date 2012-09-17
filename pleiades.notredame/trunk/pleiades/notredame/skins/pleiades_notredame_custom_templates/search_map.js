97a4077413fa85815d49d70b38e6e9e1443b79f4
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2012-09-17 21:48:07 +0000

    Add method to reset the map zoom to original extent of the batch.

--- pleiades.notredame/trunk/pleiades/notredame/skins/pleiades_notredame_custom_templates/search_map.js
+++ pleiades.notredame/trunk/pleiades/notredame/skins/pleiades_notredame_custom_templates/search_map.js
@@ -1,3 +1,57 @@
+PLZoom = L.Control.Zoom.extend({
+    onAdd: function (map) {
+        var className = 'leaflet-control-zoom',
+            container = L.DomUtil.create('div', className);
+        this._map = map;
+        this._createButton('Zoom reset', 
+            className + '-reset', 
+            container, 
+            this._zoomReset, 
+            this );
+        this._createButton('Zoom in', 
+            className + '-in', 
+            container, 
+            this._zoomIn, 
+            this );
+        this._createButton('Zoom out', 
+            className + '-out', 
+            container, 
+            this._zoomOut, 
+            this );
+        return container;
+    },
+
+    _zoomReset: function (e) {
+        this._map.fitBounds(this.options.initialBounds); 
+    },
+
+    _zoomIn: function (e) {
+        this._map.zoomIn(e.shiftKey ? 3 : 1);
+    },
+
+    _zoomOut: function (e) {
+        this._map.zoomOut(e.shiftKey ? 3 : 1);
+    },
+
+    _createButton: function (title, className, container, fn, context) {
+        var link = L.DomUtil.create('a', className, container);
+        link.href = '#';
+        link.title = title;
+        L.DomEvent
+            .on(link, 'click', L.DomEvent.stopPropagation)
+            .on(link, 'mousedown', L.DomEvent.stopPropagation)
+            .on(link, 'dblclick', L.DomEvent.stopPropagation)
+            .on(link, 'click', L.DomEvent.preventDefault)
+            .on(link, 'click', fn, context);
+        return link;
+    }
+});
+
+pl_zoom = function (options) {
+    return new PLZoom(options);
+};
+
+
 function getJSON(rel) {
   var documentNode = document;
   var linkNode = documentNode.evaluate(
@@ -17,11 +71,13 @@ function getJSON(rel) {
 }
 
 var where = getJSON("where");
-var bounds = where.bbox;
+//var bounds = where.bbox;
+var bounds = [
+  [where.bbox[1]-0.125, where.bbox[0]-0.125],
+  [where.bbox[3]+0.125, where.bbox[2]+0.125] ];
 
-var map = L.map('map', {maxZoom: 12}).fitBounds([
-  [bounds[1]-0.125, bounds[0]-0.125],
-  [bounds[3]+0.125, bounds[2]+0.125] ]);
+var map = L.map('map', {maxZoom: 12, zoomControl: false}).fitBounds(bounds);
+pl_zoom({initialBounds: bounds}).addTo(map);
 
 var terrain = L.tileLayer(
     'http://api.tiles.mapbox.com/v3/sgillies.map-ac5eaoks/{z}/{x}/{y}.png', {
@@ -30,6 +86,11 @@ var terrain = L.tileLayer(
 terrain.addTo(map);
 
 /* Not added by default, only through user control action */
+var streets = L.tileLayer(
+    'http://api.tiles.mapbox.com/v3/sgillies.map-pmfv2yqx/{z}/{x}/{y}.png', {
+        attribution: "ISAW, 2012"
+        });
+
 var imperium = L.tileLayer(
     'http://static.ahlfeldt.se/srtm/imperium/{z}/{x}/{y}.png', {
         attribution: "Johan Ahlfeldt, 2012",
@@ -38,8 +99,9 @@ var imperium = L.tileLayer(
         });
 
 L.control.layers({
-    "Pelagios": imperium,
-    "Pleiades (default)": terrain
+    "Terrain (default)": terrain,
+    "Streets": streets,
+    "Imperium": imperium,
     }).addTo(map);
 
 function setupFeature(f) {
