ae5528005d197d74e681115e42d49b53731c9a2f
David Glick <david@glicksoftware.com>
2016-04-05 10:38:32 -0700

    fix Advanced Search link, refs isawnyu/pleiades-gazetteer#86

--- pleiades/notredame/browser/__init__.py
+++ pleiades/notredame/browser/__init__.py
@@ -1,1 +1,3 @@
-#
+# Change advanced search link
+from plone.app.portlets.portlets.search import Renderer
+Renderer.action = 'search_form'
--- pleiades/notredame/browser/search.py
+++
@@ -1,9 +0,0 @@
-"""
-Override the standard Plone search view with a Pleiades-specific one.
-"""
-
-from Products.Five import BrowserView
-
-
-class PleiadesSearch(BrowserView):
-    pass
